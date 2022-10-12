b451196c8be54782230eae5f2f1782bb18d578d2
tom <tom@croll.fr>
2022-10-12 00:07:35 -0300

    fix build bugs

--- fleek.sh
+++ fleek.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "auto-install-peers=true" > .npmrc 
+echo "node-linker=pnp" >> .npmrc
+echo "symlink=false" >> .npmrc
 npm i --location=global npm 
 npm i --location=global pnpm 
 pnpm i 
@@ -7,3 +9,4 @@ pnpm fetch --prod
 pnpm install -r --offline --prod 
 pnpm build
 rm -rf node_modules
+node serve index.js
